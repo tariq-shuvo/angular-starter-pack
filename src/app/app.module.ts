bb289adcea2696fc79146cba101de1e88cb93d46
tariqul islam shuvo <tariq10043@gmail.com>
2023-05-26 13:01:07 +0600

    basic routing is completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { ContactComponent } from './components/contact/contact.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+const appRoutes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "contact-us",
+    component: ContactComponent
+  }
+]
 
 @NgModule({
   // load all the components here
   declarations: [
     AppComponent,
+    ContactComponent,
+    HomeComponent,
   ],
   // load all the global imports 
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   // load all the project providers here
   providers: [],
